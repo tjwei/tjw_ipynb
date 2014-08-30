479ad56d184b9b98eed105bd4d120046bb51f6f4
TJ Wei <tzerjen@gmail.com>
2014-08-30 17:50:40 +0800

    even better solution

--- a.c
+++ a.c
@@ -1,12 +1,14 @@
-/*Use either
-$ sha384sum a.c | xxd -r -p>a.out&& chmod a+x a.out
+/*
+It doesn't matter whether you use vim, emacs, or echo.
+Real programmers use sha384 to compile their source.
+To compile this file, either
+$ sha384sum a.c|xxd -r -p>a.out&&chmod a+x a.out
 or
-$ gcc a.c
-to compile this file and then try
+$ gcc a.c 
+ to compile this file and then try
 $ ./a.out
 */
 int main(int argc,char **argv)
 {
 return 0;
 } 
- 
