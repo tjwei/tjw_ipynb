c74e67e43783bd290324c963dc05fee0187e6500
TJ Wei <tzerjen@gmail.com>
2014-08-30 17:41:03 +0800

    add RealMan source

--- a.c
+++ a.c
@@ -1,11 +1,12 @@
 /*Use either
-$ sha384sum a.c |xxd -r -p>a.out &&chmod a+x a.out 
-or
+$ sha384sum a.c| xxd -r -p>a.out &&chmod a+x a.out
+or 
 $ gcc a.c
-to compile this file
- and then try
-$ ./a.out*/
-int main(int argc, char **argv)
-{ 
+ to compile this file
+and then try
+$ ./a.out 
+*/
+int main(int argc,char **argv)
+{
 return 0;
 }
