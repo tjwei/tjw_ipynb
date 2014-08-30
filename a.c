ec519b600558a619ce2fd9924b1bad3481162de0
TJ Wei <tzerjen@gmail.com>
2014-08-31 04:34:11 +0800

    update

--- a.c
+++ a.c
@@ -1,14 +1,17 @@
-/* 
+/*
 It doesn't matter you use vim, emacs, or echo.
 Real programmers use one-way hash functions to compile their source code.
-To compile this file, either
-$ sha384sum a.c|xxd -r -p >a.out &&chmod a+x a.out
+This file can be compiled to executable binary by gcc or SHA384.
+To get this file,  
+$ wget https://raw.githubusercontent.com/tjwei/tjw_ipynb/master/0.c
+To compile this file, either 
+$ sha384sum a.c|xxd -r -p>a.out&&chmod a+x a.out 
 or
-$ gcc a.c
+$ gcc a.c 
 Then try
-$ ./a.out 
+$ ./a.out
  to execute the binary.
-*/ 
+*/
 int main(int argc,char **argv)
 {
 return 0;
