8f84b6c64d266f956c4782b055df5c721eca5118
TJ Wei <tzerjen@gmail.com>
2014-08-30 18:04:55 +0800

    better english

--- a.c
+++ a.c
@@ -1,16 +1,15 @@
-/*
+/* 
 It doesn't matter you use vim, emacs, or echo.
-Real programmers use sha384 to compile their source code.
+Real programmers use one-way hash functions to compile their source code.
 To compile this file, either
-$ sha384sum a.c|xxd -r -p>a.out&&chmod a+x a.out
+$ sha384sum a.c|xxd -r -p >a.out &&chmod a+x a.out
 or
 $ gcc a.c
-Then try 
-$ ./a.out
+Then try
+$ ./a.out 
  to execute the binary.
 */ 
-int main(int argc, char **argv)
+int main(int argc,char **argv)
 {
 return 0;
 }
- 
