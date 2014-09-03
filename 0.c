fba9f6d6ef6b5afdf8b0dc82b5bfbb24d90c9a0d
TJ Wei <tzerjen@gmail.com>
2014-09-03 15:42:25 +0800

    update

--- 0.c
+++ 0.c
@@ -1,17 +1,17 @@
-#include <stdio.h> /* 
-echo "Hello world!" << } && exit
+#include <stdio.h>/* 
+echo "Hello world!"<< } && exit
+ 
+It doesn't matter you use vim, emacs, or echo. 
+Real programmers use one-way functions to compile programs. 
 
-It doesn't matter you use vim, emacs, or echo.
-Real programmers use one-way functions to compile programs.
- 
-$ sh 0.c
-$ gcc 0.c&&./a.out
-$ sha384sum 0.c| xxd -r -p >a.out &&chmod a+x a.out 
+$ sh 0.c 
+$ gcc 0.c &&./a.out
+$ sha384sum 0.c | xxd -r -p>a.out && chmod a+x a.out
 $ ./a.out 
- 
-sha384 was chosen because we can fit an ELF in the digest!(48>45) 
-*/
-main() {
- puts("Hello world!");
+
+sha384 was chosen
+because we can fit an ELF in the digest!(48>45)*/
+main() { 
+puts("Hello world!");
 }
  
