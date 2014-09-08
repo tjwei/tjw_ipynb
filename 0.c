1b1d3429163f40249ec6efab4126fd3f1d21bded
TJ Wei <tzerjen@gmail.com>
2014-09-09 07:10:42 +0800

    solution works for bash, zsh, tcsh on linux, osx, cygwin

--- 0.c
+++ 0.c
@@ -1,24 +1,6 @@
-#include <stdio.h>/* 
-echo "Hello world!" << '}'&& exit 
- 
-It doesn't matter you use vim, emacs, or echo. 
-Real programmers use one-way functions to compile programs.
- 
-Usage: 
-$ mkdir test 
-$ cd test 
-$ wget https://raw.githubusercontent.com/tjwei/tjw_ipynb/master/0.c
-$ sh 0.c 
-Hello World!
-$ gcc 0.c && ./a.out
-Hello World!
-$ shasum -a 384 0.c | xxd -r -p> a.out&&chmod a+x a.out
-$ ./a.out 
-Hello World!
- 
-sha384 was chosen 
-because we can fit an ELF in the digest!(48>45)
-*/ 
-main(){
-puts("Hello world!");
+#include<stdio.h>/*
+#usage: http://x.co/oDFGs83
+echo "Hello world!"<<'}'
+*/
+main(){puts("Hello world!");
 }
